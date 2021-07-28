1a50f6a9d0ce564c6b5de474aab151f6f74e3759
Victor Cequea Mijares <victor.cequeamijares.ext@bayer.com>
2021-07-28 11:48:02 -0300

    Mejoras en Explicaciones

--- HTML - CSS - JS Elementos basicos/index.js
+++ HTML - CSS - JS Elementos basicos/index.js
@@ -10,85 +10,105 @@
 /* las data types permiten permiten a javascript el entender entender 
    como debe interactuar con los elementos que vamos colocando en
    nuestras variables */
-var string = "string"; //string
-var numero = 5;        //Number
-var booleano = true;     //boolean
-var nulo = null;     //null
-var indefinido;            //undefined   
-var objecto = {}        //object    
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
-console.log(typeof(d));
-console.log(typeof(e));
-console.log(typeof(f));
 
-/* javascript evalua las condiciones de izquierda a derecha  */
+
+var string = "string";   //string
+var numero = 5;          //Number 
+var booleano = true;     //boolean
+var nulo = null;         //null
+var indefinido;          //undefined   
+var objecto = {};         //object    
+console.log(typeof(string));
+console.log(typeof(numero));
+console.log(typeof(booleano));
+console.log(typeof(nulo));
+console.log(typeof(indefinido));
+console.log(typeof(objecto));
+
+// javascript evalua las condiciones de izquierda a derecha
 console.log(string + numero);
 console.log(numero + numero + numero + string);
-console.log(numero + numero  + string + numero);
+console.log(numero + numero  + string + numero); 
 
+
+
+//OPERATORS
 
 /* 
+Aritmetica
+
++ operador de suma
+- operador de resta
+* operador de multiplicacion
+/ operador de division
+
+Agrupamiento
+
+() operador de agrupamiento, ejecuta primero lo que este ebtre los parentesis
+
+Concatenacion
+
++ operador de concatenacion, es el mismo elemento de operador de suma, pero usado con el data type string
+
+
+*/
 
 //VARIABLES
+
 /* en javascript una varible almacena informacion que puede ser cambiada 
    y usada posteriormente, esta usa la palabra reservada
-   var, let, const 
+   var, let, const
+   https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/ <--INGLES
+
+  - las variables VAR son de scope Global o de funcion, pueden ser actualizadas, re-declaradas dentro del scope y puede ser declarada sin ser Inicializada.
+  - las variables LET estan bloqueadas al scope de donde son declaradas, no pueden re-declaradas y pueden ser declaradas sin ser inicializada.
+  - las variables CONST estan bloqueadas al scope donde son declararas, no pueden ser re-declaradas ni actualizadas.  
 */
- var global = 'soy una variable global';
- let local = 'soy una variable local';
- const constante = 'una vez que me das un valor no lo voy a cambiar...bueno depende';
 
-//funciones
-function agregartext(params) {
+//CONDICIONALES
 
-}
+/* Los condicionales nos permiten 
+   controlar el comportamiento de nuestro codigo y
+   poder decidir ejecutar solo partes de nuestro codigo
+   cuando ciertas condiciones se cumplan. */
 
-function functionAlert() {
-    let a = new funcalert.alert();
-}
+   //if
+   /* if evalua una condicion entre sus parentesis, el resultado de esa evaluacion
+      siempre devuelve un resultado true o false, si el resultado en true if ejecutara
+      el codigo entre los {}   */
 
-const arrowfunction = () => {
+   if (condition) //condicion a evaluar
+   {
+       //codigo que se ejecuta si el resultado de la evaluacion es true
+   }
 
-}
-
-//array
-var array = ['Objecto 1','Objecto 2','Objecto 3',]
-
-//bucles
- for (let i = 0; i < 5; i++) {
-     console.log(i);
-}
-
-array.forEach(element => {
-    console.log(element)
-});
-
-//condicionales
-
-while (condition) {
+   //else
+   /* si el resultado de la evaluacion realizada por if es false, 
+      podemos pedirle que ejecute un bloque de codigo usando la palabra else */
     
-}
-
-if (condition) {
+   if (condition) //condicion a evaluar
+   { 
+     //codigo que se ejecuta si el resultado de la evaluacion es true  
+   } else {
+     //codigo que se ejecuta si el resultado de la evaluacion es false
+   }
+   
+   //if else
+   /* si el resultado de la evaluacion realizada por if es false, 
+      y necesitamos que hacer una nueva evaluacion podemos usar else if */
     
-} else {
-    
-}
-
-switch (key) {
-    case value:
-        
-        break;
-
-    default:
-        break;
-}
+   if (condition) //condicion a evaluar
+   { 
+     //codigo que se ejecuta si el resultado de la evaluacion es true  
+   } else if (condition){ //si la primera evaluacion es false podemos realizar una nueva evaluacion
+     //codigo que se ejecuta si el resultado de la evaluacion es false
+   } 
+   
+   
 
 
 
 
- */
+
 
 
